fd0597275933a38e68bd2876d870e4322bca100f
Amirtha <amirtha4501@gmail.com>
2020-01-14 23:14:57 +0530

    Removed unnecessary comments

--- controller.py
+++ controller.py
@@ -1,9 +1,6 @@
 from manage import User, LoggedUsers, LoggedUsersPost, LoggedUsersComment, LoggedUsersLike, db, app
 from flask import Flask, request, jsonify, redirect, url_for, render_template
 from datetime import datetime
-# import datetime
-# from jinja2 import Markup
-# from flask_moment import Moment
 import json
 
 
@@ -11,16 +8,12 @@ import json
 @app.route('/signup')
 def user_signup():
     input_data = request.get_json()
-    get_data = request.json
-    print(input_data)
-    print(get_data)
 
     first_name = input_data.get('first_name')
     last_name = input_data.get('last_name')
     email=input_data.get('email')
     mobile=input_data.get('mobile')
     password=input_data.get('password')
-    # dob = input_data.get('dob'),
     gender = input_data.get('gender')
     user_name = first_name + " " + last_name
 
@@ -45,26 +38,14 @@ def user_signup():
         print("password is already present")
         return "password is already present"
 
-    # print(name_already_existing)
-    # print(email_already_existing)
-    # print(mobile_already_existing)
-    # print(password_already_existing)
-
     admin = User(
         user_name = user_name,
-        # email=input_data.get('email'),
-        # mobile=input_data.get('mobile'),
-        # password=input_data.get('password'),
-        # # dob = input_data.get('dob'),
-        # gender = input_data.get('gender')
         email = email,
         mobile = mobile,
         password = password,
-        # dob = dob,
         gender = gender
     )
     admin.add()
-
     return redirect('/listusers')
 
 
@@ -74,7 +55,6 @@ def list_users():
     user_list = []
 
     for user in users:
-        print(user.id,user.user_name,user.email)
         dic_user = {
             "User_id" : user.id,
             "User_name" : user.user_name,
@@ -92,32 +72,16 @@ def logged_users():
 
     existing_user = User.query.get(input_data['user_id'])
 
-    # last_name = input_data.get('last_name')
-    # email=input_data.get('email')
-    # mobile=input_data.get('mobile')
-    # password=input_data.get('password')
-
-
     if not existing_user:
         print("User signup is needed to login")
         return "User signup is needed to login"
         # return redirect(url_for('user_signup'))
 
-    print(existing_user.mobile)
     already_existing = LoggedUsers.query.filter_by(user_id=input_data['user_id']).first()
-    # email_already_existing = User.query.filter_by(email=input_data['email']).first()
-    # password_already_existing = User.query.filter_by(password=input_data['password']).first()
-
     
     if already_existing:
         print("You're already logged in")
         return "You're already logged in"
-    # elif not email_already_existing:
-        # print("Email id is incorrect")
-        # return "Email id is incorrect"
-    # elif not password_already_existing:
-        # print("password is incorrect")
-        # return "Password is incorrect"
 
     log_users = LoggedUsers(
         user_id = input_data['user_id'],
@@ -125,38 +89,20 @@ def logged_users():
         user_email = existing_user.email
     )
     log_users.add()
-
-    # return redirect('register.html')
     return "logged users"
     # return redirect(url_for('post'))
+
 
 @app.route('/loggedusers/post')
 def post():
     input_message = request.get_json()
-    print(input_message)
-    # if input_message == None:
-    #     print("Input message is required to post.")
-
     login_existing = LoggedUsers.query.filter_by(user_id=input_message['user_id']).first()
-    print(login_existing)
+
     if not login_existing:
         print("Login is needed to post")
         return "Login is needed to post"
-        # moment = Moment(app)
-        # print(moment)
-        # return moment
-        # return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
-
         
     existing_user = LoggedUsers.query.get(input_message['user_id'])
-    # print(existing_user)
-    # print(existing_user.user_name)
-    # print(existing_user.id)
-    # print(input_message['message'])
-
-    # print(existing_user.user_name)
-    # now = datetime.datetime.now()
-    # print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
     log_users_post = LoggedUsersPost(
         user_id = input_message['user_id'],
@@ -165,92 +111,66 @@ def post():
         time = datetime.utcnow()
     )    
     log_users_post.add()
-    # print(time)
-
-    # return json.dumps(log_users_post.__dict__ )
     return "posted"
-    # return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
+
 
 @app.route('/loggedusers/post/comment')
 def comment():
     input_content = request.get_json()
-    print(input_content)
-
     login_existing = LoggedUsers.query.filter_by(user_id=input_content['user_id']).first()
-    print(login_existing)
     
     if not login_existing:
         print("Login is needed to comment")
         return "Login is needed to comment"
     
     posted_existing_user = LoggedUsersPost.query.get(input_content['post_id'])
-    print(posted_existing_user)
-    # print(posted_existing_user.user_id)
-    # print(posted_existing_user.user_name)
-    # print(posted_existing_user.message)
-    if posted_existing_user:
-        comment_existing = LoggedUsersComment.query.filter_by(post_id=input_content['post_id']).count()
 
-        log_users_comment = LoggedUsersComment(
-        user_id = input_content['user_id'],
-        post_id = input_content['post_id'],
-        user_name = login_existing.user_name,
-        comment = input_content['comment'],
-        comment_count = comment_existing + 1,
-        comment_time = datetime.utcnow()
-        )    
-        log_users_comment.add() 
-        return "comment"
+    if not posted_existing_user:
+        print("No such posts to comment.")
+        return "No such posts to comment."
 
-    else:
-        print("No such posts to like.")
-        return "No such posts to like."
+    comment_existing = LoggedUsersComment.query.filter_by(post_id=input_content['post_id']).count()
 
+    log_users_comment = LoggedUsersComment(
+    user_id = input_content['user_id'],
+    post_id = input_content['post_id'],
+    user_name = login_existing.user_name,
+    comment = input_content['comment'],
+    comment_count = comment_existing + 1,
+    comment_time = datetime.utcnow()
+    )    
+    log_users_comment.add() 
+    return "comment"
 
 
 @app.route('/loggedusers/post/like')
 def likes():
     input_content = request.get_json()
-    # print(input_content)
-
     login_existing = LoggedUsers.query.filter_by(user_id=input_content['user_id']).first()
-    # print(login_existing)
     
     if not login_existing:
         print("Login is needed to like")
         return "Login is needed to like"
     
     posted_existing_user = LoggedUsersPost.query.get(input_content['post_id'])
-    print(posted_existing_user)
-    # print(posted_existing_user.user_id)
-    # print(posted_existing_user.user_name)
-    # print(posted_existing_user.message)
-    if posted_existing_user:
-        # # count = 0
-        # # # if like == 0:
-        # #      count += 1
-        # # obtain = LoggedUsersLike.qu
-        # obtain = LoggedUsersLike.query.get(input_content['post_id'])
-        # # print(obtain.user_name, "obtain")
-        # if obtain == None:
-        #     obtain.like_count = 1
-        # else:
-        #     obtain.like_count = obtain.like_count + 1
-        # # like_count = obtain.like_count or 0
-        like_existing = LoggedUsersLike.query.filter_by(post_id=input_content['post_id']).count()
-
-        log_users_like = LoggedUsersLike(
-        user_id = input_content['user_id'],
-        post_id = input_content['post_id'],
-        user_name = login_existing.user_name,
-        like_count = like_existing + 1,
-        like_time = datetime.utcnow()
-        )    
-        log_users_like.add()
-        return "Liked" 
-    else:
+    
+    if not posted_existing_user:
         print("No such posts to like.")
         return "No such posts to like."
+
+    like_existing_count = LoggedUsersLike.query.filter_by(post_id=input_content['post_id']).count()
+    # liked_existing_pid = LoggedUsersLike.query.get(input_content['post_id'])
+    # print(liked_existing_pid)
+
+    log_users_like = LoggedUsersLike(
+    user_id = input_content['user_id'],
+    post_id = input_content['post_id'],
+    user_name = login_existing.user_name,
+    like_count = like_existing_count + 1,
+    like_time = datetime.utcnow()
+    )    
+    log_users_like.add()
+    return "Liked" 
 
 
 app.run(host='0.0.0.0',port=7000,debug=True)
